d25c67c96bfb2d07cfb74e04066e522f45a8d882
VaibhaviJantralwala <vaibhavijantralwala@gmail.com>
2026-03-12 12:31:24 +0530

    modified

--- src/oops/functions.java
+++ src/oops/functions.java
@@ -2,6 +2,8 @@ package oops;
 
 public class functions {
 
+	// functions and its 4 types
+	
 	// no param no return type
 	void test() {
 		System.out.println("No param no return type");
@@ -26,15 +28,15 @@ public class functions {
 	
 	public static void main(String[] args) {
 		
-		functions fun = new functions();
+		functions fun1 = new functions();
 		
-		fun.test();
+		fun1.test();
 		
-		fun.test(22);
+		fun1.test(22);
 		
-		System.out.println(fun.test1());
+		System.out.println(fun1.test1());
 		
-		System.out.println(fun.test2(20));
+		System.out.println(fun1.test2(20));
 		
 	}
 	
